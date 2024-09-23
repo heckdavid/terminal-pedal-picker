959b0f9f59cdf80f0eb2363b6282fcd0a7a16d45
David Heck <david.heck1991@gmail.com>
2024-09-23 14:55:36 -0400

    Added textarea to html and added to index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// Commands //
+begin = [
+'Categories',
+'Dry Effects View dry effects',
+'Wet Effects View wet effects'
+];
+
+categories = [
+'Categories Choose between either dry effects or wet effects'
+];
+
+dryEffects = [
+'Volume',
+'Tuner',
+'Compression',
+'Pitch',
+'Drives',
+];
+
+wetEffects = [
+'Modulation',
+'Delay',
+'Reverb'
+];
